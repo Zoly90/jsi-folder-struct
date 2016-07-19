0c2e8d0574184417033485836ac3ed1d8ba998d4
Zoly90 <zoltan.incze@evozon.com>
2016-07-19 16:22:28 +0300

    ex html perfect view

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-const folders = directories: [
+
+
+const folders =
       {
         type: 'dir',
         name: 'app',
@@ -55,4 +57,53 @@ const folders = directories: [
           }
         ]
       }
-    ]
+
+
+function buildList(data, isSub){
+  debugger;
+  var html = (isSub)?'<div>':''; // Wrap with div if true
+  html += '<ul>';
+  for(var item in data) {
+    if (item != "type" && isNaN(item)) {
+      break;
+    }
+    else{
+      if (item === "type" || typeof(data[item]) === 'object') {
+        html += '<li ';
+        
+        if (data.type === "dir" || data[item].type === "dir") {
+          html += 'class="folder-item"';
+        } else  {
+          html += 'class="file-item"';
+        }
+
+        html += '>';
+
+        if (data.type === "dir" || data[item].type === "dir") { // An array will return 'object'
+          if (isSub) {
+            html += data[item].name;
+            html += '</li>';
+            if (data[item].children.constructor === Array)
+              html += buildList(data[item].children, true);
+          }
+          else {
+            html += data.name;
+            html += '</li>';
+            if (data.children.constructor === Array)
+              html += buildList(data.children, true);
+          }
+        }
+        else {
+          html += data[item].name;
+          html += '</li>'; // No submenu
+        }
+        //html += '</li>';
+      }
+    }
+  }
+  html += '</ul>';
+  html += (isSub)?'</div>':'';
+  return html;
+}
+
+document.getElementById('folder').innerHTML = buildList(folders, false);
