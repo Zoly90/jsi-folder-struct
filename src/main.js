942765aee8f4ae440042d2d3ac8faf0a868aa3cc
Zoly90 <zoltan.incze@evozon.com>
2016-07-20 15:50:14 +0300

    folder - file structer WORKING

--- src/main.js
+++ src/main.js
@@ -58,9 +58,93 @@ const folders =
         ]
       }
 
+function readInput(){
+  var inputForFilter = document.getElementById("filterInput").value;
+  return inputForFilter;
+}
+
+var filteredList = new Array();
+
+function buildFilteredList(data, input) {
+
+  //console.log(input);
+  //debugger;
+  var filteredElement;
+  if (input === "app" || input === "pp" || input === "p")
+    return data;
+  else {
+    for (var item in data) {
+      if (item != "type" && isNaN(item)) {
+        break;
+      }
+      else {
+        if (data.type === "dir") {
+          if (data.name.indexOf(input) > -1) {
+            /*filteredElement = new Object();
+            filteredElement.type = data.type;
+            filteredElement.name = data.name;
+            if (data.children.constructor === Array) {
+              filteredElement.children = data.children;
+              filteredList.push(filteredElement);
+              buildFilteredList(data.children, input);
+            }*/
+            return data;
+          }
+          buildFilteredList(data.children, input);
+        }
+        else {
+          if (data[item].type === "dir") {
+            if (data[item].name.indexOf(input) > -1) {
+              filteredElement = new Object();
+              filteredElement.type = data[item].type;
+              filteredElement.name = data[item].name;
+              filteredElement.children = takeEverythingFromFolder(data[item].children);
+              return filteredList.push(filteredElement);
+            }
+            buildFilteredList(data[item].children, input);
+          }
+          else if (data[item].name.indexOf(input) > -1) {
+            filteredElement = new Object();
+            filteredElement.type = data[item].type;
+            filteredElement.name = data[item].name;
+            filteredList.push(filteredElement);
+          }
+        }
+
+      }
+
+    }
+    return filteredList;
+  }
+  //console.log(filteredList);
+
+  //}
+}
+
+
+function takeEverythingFromFolder (data){
+  //debugger;
+  var folderSituation = new Array()
+  for (var itemsInFolder in data){
+    if (data[itemsInFolder].type === "dir") {
+      var fileObject = new Object();
+      fileObject.type = data[itemsInFolder].type;
+      fileObject.name = data[itemsInFolder].name;
+      fileObject.children = takeEverythingFromFolder(data[itemsInFolder].children);
+      folderSituation.push(fileObject);
+    }
+    else{
+      var fileObject = new Object();
+      fileObject.type = data[itemsInFolder].type;
+      fileObject.name = data[itemsInFolder].name;
+      folderSituation.push(fileObject);
+    }
+  }
+  return folderSituation;
+}
 
 function buildList(data, isSub){
-  debugger;
+  //debugger;
   var html = (isSub)?'<div>':''; // Wrap with div if true
   html += '<ul>';
   for(var item in data) {
@@ -68,42 +152,66 @@ function buildList(data, isSub){
       break;
     }
     else{
-      if (item === "type" || typeof(data[item]) === 'object') {
-        html += '<li ';
-        
-        if (data.type === "dir" || data[item].type === "dir") {
-          html += 'class="folder-item"';
-        } else  {
-          html += 'class="file-item"';
-        }
+      if (data[item].constructor === Array)
+          html += buildList(data[item], true);
+      else {
+        if (item === "type" || typeof(data[item]) === 'object') {
+          html += '<li ';
 
-        html += '>';
+          if (data.type === "dir" || data[item].type === "dir") {
+            html += 'class="folder-item"';
+          } else {
+            html += 'class="file-item"';
+          }
 
-        if (data.type === "dir" || data[item].type === "dir") { // An array will return 'object'
-          if (isSub) {
-            html += data[item].name;
-            html += '</li>';
-            if (data[item].children.constructor === Array)
-              html += buildList(data[item].children, true);
+          html += '>';
+
+          if (data.type === "dir" || data[item].type === "dir") { // An array will return 'object'
+            if (isSub) {
+              html += data[item].name;
+              html += '</li>';
+              if (data[item].children.constructor === Array)
+                html += buildList(data[item].children, true);
+            }
+            else {
+
+              if (item === "type") {
+                html += data.name;
+                html += '</li>';
+                if (data.children.constructor === Array)
+                  html += buildList(data.children, true);
+              }
+              else {
+                html += data[item].name;
+                html += '</li>';
+                if (data[item].children.constructor === Array)
+                  html += buildList(data[item].children, true);
+              }
+            }
           }
           else {
-            html += data.name;
-            html += '</li>';
-            if (data.children.constructor === Array)
-              html += buildList(data.children, true);
+            html += data[item].name;
+            html += '</li>'; // No submenu
           }
+          //html += '</li>';
         }
-        else {
-          html += data[item].name;
-          html += '</li>'; // No submenu
-        }
-        //html += '</li>';
       }
     }
   }
   html += '</ul>';
   html += (isSub)?'</div>':'';
+  console.log(html);
   return html;
 }
 
 document.getElementById('folder').innerHTML = buildList(folders, false);
+
+function search(){
+  var string = readInput();
+  if(string === "")
+    document.getElementById('folder').innerHTML = buildList(folders, false);
+  else{
+    filteredList = new Array();
+    document.getElementById('folder').innerHTML = buildList(buildFilteredList(folders, string), false);
+  }
+}
